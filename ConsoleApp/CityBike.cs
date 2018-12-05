83891edf8922229a7f07caadafb97215b01f60e5
neberd <masterneberd@yahoo.no>
2018-12-05 12:28:05 +0100

    Initial commit

---
+++ ConsoleApp/CityBike.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp
+{
+    public class CityBike
+    {
+        public Station[] Stations { get; set; }
+    }
+    public class Station
+    {
+        public int Id { get; set; }
+        public Availability Availability { get; set; }
+        public string Title { get; set; }
+    }
+    public class Availability
+    {
+        public int Bikes { get; set; }
+        public int Locks { get; set; }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static readonly string ApiKey = "";
+        public static readonly string StationsData = "stations";
+        public static readonly string AvailabilityData = "stations/availability";
+
+        private static readonly IList<Station> StationList = new List<Station>();
+        private static readonly IList<Station> AvailabilityList = new List<Station>();
+
+        public static void Main(string[] args)
         {
+            if (string.IsNullOrWhiteSpace(ApiKey))
+            {
+                Console.Out.WriteLine("Mangler API-nøkkel!");
+            }
+            else
+            {
+                RunAsync(StationsData, StationList).Wait();
+                RunAsync(AvailabilityData, AvailabilityList).Wait();
+                Display();
+            }
+        }
+
+        private static void Display()
+        {
+            if (StationList.Count == 0 || AvailabilityList.Count == 0)
+            {
+                Console.WriteLine("Fant ingen data");
+                return;
+            }
+            var data = (from s in StationList
+                        join a in AvailabilityList on s.Id equals a.Id
+                        select new { s.Id, s.Title, a.Availability.Bikes, a.Availability.Locks }).ToList();
+
+            var table = new ConsoleTable("Id", "Navn", "Ledige sykler", "Tilgjengelige låser");
+            foreach (var d in data)
+            {
+                table.AddRow(d.Id, d.Title, d.Bikes, d.Locks);
+            }
+
+            Console.BufferHeight = 500;
+            Console.WriteLine(table);
+            Console.ReadLine();
+        }
+
+        private static async Task RunAsync(string apiMethod, ICollection<Station> stationList)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://oslobysykkel.no/api/v1/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Add("Client-Identifier", ApiKey);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync(apiMethod);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        CityBike cityBike = await response.Content.ReadAsAsync<CityBike>();
+                        foreach (Station station in cityBike.Stations)
+                        {
+                            stationList.Add(station);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
